c68f7e196bd912dbb1ef17986e848a6c01d2760b
emanoelCarvalho <ehcs@discente.ifpe.edu.br>
2025-01-08 11:30:16 -0300

    feat: Change secret key generation route from POST to GET

--- src/routes/secretKeyRoutes.js
+++ src/routes/secretKeyRoutes.js
@@ -2,6 +2,6 @@ const express = require("express");
 const { generateSecretKey } = require("../controllers/secretKeyControllers");
 
 const router = express.Router();
-router.post("/generate-secret-key", generateSecretKey);
+router.get("/generate-secret-key", generateSecretKey);
 
 module.exports = router;
