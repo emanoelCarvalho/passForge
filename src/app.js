d014a22adeb0773eda67ca6396957b883da71a87
emanoelCarvalho <ehcs@discente.ifpe.edu.br>
2025-01-15 20:16:39 -0300

    :sparkles: feat: Configure Swagger options for Password Manager API documentation

--- src/app.js
+++ src/app.js
@@ -1,19 +1,31 @@
 const express = require("express");
 
-const swaggerUi = require('swagger-ui-express');
-const swaggerJsdoc = require('swagger-jsdoc') ;
+const swaggerUi = require("swagger-ui-express");
+const swaggerJsdoc = require("swagger-jsdoc");
 
 const passwordRoutes = require("./routes/passwordRoutes");
 const secretKeyRoutes = require("./routes/secretKeyRoutes");
 
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Password Manager API",
+      version: "1.0.0",
+      description: "Documentation for Password Manager API",
+    },
+  },
+
+  apis: ["./routes/*.js"],
+};
 const app = express();
 
 app.use(express.json());
 
-app.use('/api', passwordRoutes);
-app.use('/api', secretKeyRoutes);
+app.use("/api", passwordRoutes);
+app.use("/api", secretKeyRoutes);
 
 const PORT = 3000;
 app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`);
-})
+  console.log(`App listening on port ${PORT}`);
+});
