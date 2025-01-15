4bde0d4fabf9a05d7acaa1ec17ceb1d240b553e4
emanoelCarvalho <ehcs@discente.ifpe.edu.br>
2025-01-15 20:28:40 -0300

    :boom: fix: Revertendo mudanças ineficientes

--- src/app.js
+++ src/app.js
@@ -1,35 +1,15 @@
 const express = require("express");
-
-const swaggerUi = require("swagger-ui-express");
-const swaggerJsdoc = require("swagger-jsdoc");
-
 const passwordRoutes = require("./routes/passwordRoutes");
 const secretKeyRoutes = require("./routes/secretKeyRoutes");
 
 const app = express();
 
-const options = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "Password Manager API",
-      version: "1.0.0",
-      description: "Documentation for Password Manager API",
-    },
-  },
-
-  apis: ["./routes/*.js"],
-};
-
-const swaggerSpec = swaggerJsdoc(options);
-app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-
 app.use(express.json());
 
-app.use("/api", passwordRoutes);
-app.use("/api", secretKeyRoutes);
+app.use('/api', passwordRoutes);
+app.use('/api', secretKeyRoutes);
 
 const PORT = 3000;
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-});
+    console.log(`App listening on port ${PORT}`);
+})
