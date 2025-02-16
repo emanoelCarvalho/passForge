69335882d448e0689ad97f0191579a5d944d7641
emanoelCarvalho <ehcs@discente.ifpe.edu.br>
2025-02-15 21:11:53 -0300

    :fire: chore: Remove Swagger UI integration from app.js as part of API documentation cleanup

--- src/app.js
+++ src/app.js
@@ -1,8 +1,5 @@
 const express = require("express");
 
-const swaggerUi = require("swagger-ui-express"); 
-const swaggerFile = require("../swagger_output.json");
-
 const passwordRoutes = require("./routes/passwordRoutes");
 const secretKeyRoutes = require("./routes/secretKeyRoutes");
 const config = require("./services/configServices");
@@ -15,7 +12,6 @@ const app = express();
 app.use(express.json());
 app.use("/api", passwordRoutes);
 app.use("/api", secretKeyRoutes);
-app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.listen(PORT, () => {
     log(`Server is running on port ${PORT}`);
